cfd8af6aca7f4235a54ba77d1f576557db731d9e
Victor Queiroz <victorcqueirozg@gmail.com>
2023-07-25 00:59:21 -0300

    remove commented code

--- index.ts
+++ index.ts
@@ -4,20 +4,6 @@ import Effect, { IStore } from "./Effect";
 export type { IStore } from "./Effect";
 export { default as Effect } from "./Effect";
 
-// export interface IEffectEventMap<Action> {
-//   action: Action;
-// }
-
-// export type StoreAction<T> = T extends IStore<unknown, infer Actions> ? Actions : never;
-
-// export interface IEffect<Store extends IStore> {
-//   store: Store;
-//   onAction(
-//     action: StoreAction<Store>
-//   ): Promise<void>;
-//   addEffect(value: IEffect<Store>): void;
-// }
-
 export default function effect<Store extends IStore>(
   effect: Effect<Store>,
   {
