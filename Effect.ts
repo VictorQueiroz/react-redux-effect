ae173a8e7fc3a92688d35f02f4461c5ec3b5c509
Victor Queiroz <victorcqueirozg@gmail.com>
2023-07-30 01:58:31 -0300

    add support for task tracking actions

--- Effect.ts
+++ Effect.ts
@@ -4,17 +4,19 @@ export interface IAction {
   type: string;
 }
 
-export interface IStore<S = unknown, A extends IAction = IAction> {
+export type ActionLike = IAction | IBeginTaskAction | IFinishTaskAction;
+
+export interface IStore<S = unknown, A extends ActionLike = ActionLike> {
   getState(): S;
   dispatch(action: A): void;
 }
 
-export interface IEffectEventMap<Store extends IStore> {
-  action: StoreAction<Store>;
+export interface IEffectEventMap {
+  action: ActionLike;
   unhandledRejection: unknown;
 }
 
-export type StoreAction<T> = T extends IStore<any, infer Actions>
+export type StoreAction<T> = T extends IStore<unknown, infer Actions>
   ? Actions
   : never;
 
@@ -24,9 +26,40 @@ export interface IEffect<Store extends IStore> {
   addEffect(value: IEffect<Store>): void;
 }
 
-export default abstract class Effect<Store extends IStore> extends EventEmitter<
-  IEffectEventMap<Store>
-> {
+const TaskIdProp = Symbol("reduxEffectTaskId");
+
+export interface IEffectTaskAction {
+  [TaskIdProp]: number;
+  type: string;
+}
+
+function finishTask(taskId: number): IFinishTaskAction {
+  return {
+    type: "@@ReduxEffectActionType/FinishTaskAction",
+    [TaskIdProp]: taskId,
+  };
+}
+
+export interface IFinishTaskAction extends IEffectTaskAction {
+  type: "@@ReduxEffectActionType/FinishTaskAction";
+}
+
+function beginTask<T extends IEffectTaskAction>(action: T): IBeginTaskAction {
+  return {
+    action,
+    type: "@@ReduxEffectActionType/BeginTaskAction",
+    [TaskIdProp]: action[TaskIdProp],
+  };
+}
+
+export interface IBeginTaskAction extends IEffectTaskAction {
+  type: "@@ReduxEffectActionType/BeginTaskAction";
+  action: IAction;
+}
+
+export default abstract class Effect<
+  Store = IStore
+> extends EventEmitter<IEffectEventMap> {
   readonly #children;
   #pending = Promise.resolve();
   public constructor() {
@@ -35,17 +68,28 @@ export default abstract class Effect<Store extends IStore> extends EventEmitter<
   }
   public run(action: StoreAction<Store>) {
     if (this.shouldProcessAction(action)) {
+      const requiresFinishedActions = TaskIdProp in action;
+      const taskId = requiresFinishedActions ? action[TaskIdProp] : null;
+      if (requiresFinishedActions) {
+        this.dispatch(beginTask(action));
+      }
       this.#pending = this.#pending.then(() =>
-        this.onAction(action).catch((reason) => {
-          this.emit("unhandledRejection", reason);
-        })
+        this.onAction(action)
+          .catch((reason) => {
+            this.emit("unhandledRejection", reason);
+          })
+          .finally(() => {
+            if (taskId !== null) {
+              this.dispatch(finishTask(taskId));
+            }
+          })
       );
     }
     for (const child of this.#children) {
       child.run(action);
     }
   }
-  public dispatch(action: StoreAction<Store>) {
+  public dispatch(action: ActionLike) {
     this.emit("action", action);
   }
   public add(effect: Effect<Store>) {
